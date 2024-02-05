9b01ac7fd485c33af88b000501c285489d7bcd60
root <root@Zaks-PC>
2024-02-05 19:04:59 +0000

    added file I/O and fixed bugs

--- papyrus.c
+++ papyrus.c
@@ -3,51 +3,63 @@
 
 #define clear() printf("\e[1;1H\e[2J")
 
-void start()
+char fileName[100];
+FILE *file;
+int lineNum = 1;
+
+int open()
 {
-	int lineNum = 1;
-	clear();
-        printf("      Papyrus\n      ---------------\n");
-        printf("      %d| ", lineNum);
-        while (1)
+    clear();
+        // Get the filename from the user
+    printf("Enter filename to save: ");
+    scanf("%s", fileName);
+    getchar(); // Consume the \n so it isn't read into buffer
+	// Print the initial UI
+    printf("      Papyrus\n      ---------------\n");
+    printf("      1| ");
+
+    // Open the file in write mode
+    file = fopen(fileName, "w");
+
+    if (file == NULL)
+    {
+        printf("Error opening file.\n");
+        exit(1);
+    }
+}
+
+void edit()
+{
+    while (1)
+    {
+        int ch = getchar();
+
+        if (ch == EOF || ch == 4) // EOF or Ctrl-D to exit the loop
         {
-                if (getchar() == '\n')
-                {
-                        lineNum++;
-                        
-			if (lineNum < 10)
-                        {
-                        printf("      %d| ", lineNum);
-                        }
-                        if (lineNum >= 10 && lineNum < 100)
-                        {
-                        printf("     %d| ", lineNum);
-                        }
-                        if (lineNum >= 100 && lineNum < 1000)
-                        {
-                        printf("    %d| ", lineNum);
-                        }
-                        if (lineNum >= 1000 && lineNum < 10000)
-                        {
-                        printf("   %d| ", lineNum);
-                        }
-                        if (lineNum >= 10000 && lineNum < 100000)
-                        {
-                        printf("  %d| ", lineNum);
-                        }
-                        if (lineNum >= 100000 && lineNum < 1000000)
-                        {
-                        printf(" %d| ", lineNum);
-                        }
-                        if (lineNum >= 1000000 && lineNum < 10000000)
-                        {
-                        printf("%d| ", lineNum);
-                        }
-                }
-	}
+	    printf("\n---File saved---\n\n");
+            break;
+        }
+        else if (ch == '\n')
+        {
+            lineNum++;
+            fprintf(file, "\n");		// Output this to file
+            printf("      %d| ", lineNum);	// Output this to terminal
+        }
+        else
+        {
+            // Write the character to the file
+            fputc(ch, file);
+        }
+    }
+
+    // Close the file when done
+    fclose(file);
 }
 
 int main()
 {
-	start();
+    open();
+    edit();
+    return 0;
 }
+
